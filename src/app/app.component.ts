6511890fba8ebb63caf10daa8274a01b1612190e
Keir Staple <keirstaple@comcast.net>
2016-09-22 17:55:55 -0700

    experiment adding ngModel for comic object

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+export class Comic{
+  id: number;
+  title: string;
+  author: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
+  title = 'Foster Comics';
+  comic: Comic = {
+    id: 1;
+    title: 'Slice Vol. 1';
+    author: 'Foster Woodburn';
+  };
 }
